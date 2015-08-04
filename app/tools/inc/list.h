265481c7b6c1af26026b7de5b4c57a1c82a5061b
KarsusNeko <karsus91@gmail.com>
2015-08-04 18:43:06 +0800

    150804-2

--- app/tools/inc/list.h
+++ app/tools/inc/list.h
@@ -1,6 +1,8 @@
 #ifndef _LIST_H
 #define _LIST_H
 
+enum {RMV_SUCC, LST_EMPTY, NO_ELEM};
+
 typedef struct _list_node
 {
 	struct _list_node*	next;
@@ -12,9 +14,12 @@ typedef struct _list
 	list_node*			tail;
 } list;
 
+typedef int (*compar)(list_node* ln1, list_node* ln2);
+
 void list_init(list* l);
 //void list_free(list* l);
-void list_append(list_node* ln);
-//void list_remove();
+list_node* list_find(list* l, list_node* ln);
+void list_append(list* l, list_node* ln);
+int list_remove(list* l, list_node* ln, compar func);
 
 #endif // __LIST_H
--- app/tools/src/list.c
+++ app/tools/src/list.c
@@ -6,6 +6,9 @@ void list_init(list* l)
 	l->tail	= NULL;
 }
 
+list_node* list_find(list* l, list_node* ln)
+{}
+
 void list_append(list* l, list_node* ln)
 {
 	if(l->head == NULL && l->tail == NULL)
@@ -18,5 +21,23 @@ void list_append(list* l, list_node* ln)
 		ln->next		= NULL;
 		l->tail->next	= ln;
 		l->tail			= ln;
+	}
+}
+
+int list_remove(list* l, list_node* ln, compar func)
+{
+	list* tmp = l->head;
+	if(tmp == NULL)
+		return 2;
+
+	while(tmp != NULL)
+	{
+		if(compar(tmp->next, ln))
+		{
+			tmp = tmp->next;
+		}
+		else
+		{
+		}
 	}
 }
