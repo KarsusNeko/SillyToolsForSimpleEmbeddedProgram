9bf400bfe72b0a0c9ff88e4611b443cc8aa3c1d1
Karsus <karsus91@gmail.com>
2015-08-11 11:47:46 +0800

    150811-2
    update list component

--- app/tools/src/list.c
+++ app/tools/src/list.c
@@ -6,12 +6,27 @@ void list_init(list* l)
 	l->tail	= NULL;
 }
 
-list_node* list_find(list* l, list_node* ln)
-{}
+list_node* list_find(list* l, list_node* ln, compar func)
+{
+	list_node* tmp = l->head;
+	
+	if(tmp == NULL)
+		return NULL;
+	
+	if(!func(ln, tmp))
+		return tmp;
+	
+	while(tmp->next != NULL)
+	{
+		if(!func(ln, tmp->next))
+			return tmp->next;
+		tmp = tmp->next;
+	}
+}
 
 void list_append(list* l, list_node* ln)
 {
-	if(l->head == NULL && l->tail == NULL)
+	if(l->head == NULL)
 	{
 		l->head			= ln;
 		l->tail			= ln;
@@ -24,20 +39,55 @@ void list_append(list* l, list_node* ln)
 	}
 }
 
-int list_remove(list* l, list_node* ln, compar func)
+list_node* list_remove(list* l, list_node* ln, compar func)
 {
-	list* tmp = l->head;
-	if(tmp == NULL)
-		return 2;
+	list_node* tmp	= l->head;
+	list_node* rmv;
 
-	while(tmp != NULL)
+	if(tmp == NULL)
+		return tmp;
+
+	if(!func(ln, tmp))
 	{
-		if(compar(tmp->next, ln))
+		l->head	= NULL;
+		l->tail	= NULL;
+		return tmp;
+	}
+
+	while(tmp->next != NULL)
+	{
+		if(func(ln, tmp->next) != 0)
 		{
 			tmp = tmp->next;
 		}
 		else
 		{
+			rmv = tmp->next;
+			if(rmv->next)
+			{
+				tmp->next = rmv->next;
+			}
+			else
+			{
+				tmp->next = NULL;
+				l->tail = tmp;
+			}
+			return rmv;
 		}
 	}
+
+	return NULL;
+}
+
+void list_travel(list* l, info print_info)
+{
+	list_node* tmp = l->head;
+
+	if(tmp == NULL)
+		return;
+	do
+	{
+		print_info(tmp);
+		tmp = tmp->next;
+	}while(tmp != NULL);
 }
